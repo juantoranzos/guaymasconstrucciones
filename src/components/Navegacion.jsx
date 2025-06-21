d29e3483b222b34ef0db788e54268664035a91ed
Juanchi <101830727+juantoranzos@users.noreply.github.com>
2025-06-21 20:10:41 -0300

    Add responsive styles to navigation

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -3,95 +3,48 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { BsBuilding } from "react-icons/bs";
+import "./Navegacion.css";
 
 export const Navegacion = () => {
   return (
-    <Navbar
-      expand="lg"
-      style={{ backgroundColor: "#0b0f2c" }}
-      className="p-4 "
-    >
+    <Navbar expand="lg" className="p-4 navbar-custom">
       <Container>
         <Navbar.Brand
           href="#"
           className="d-flex align-items-center text-white"
-          style={{ fontSize: "2rem", fontWeight: "bold" }} // Aumenta el tamaño del texto
         >
           <BsBuilding className=" text-white p-2" size={42} />  CONSTRUCCIONES CIVILES
         </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
-          style={{ backgroundColor: "white" }}
-        />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto flex-wrap">
             <Nav.Link
               href="#servicios"
               className="text-white linksitos"
-              style={{
-                fontSize: "1rem",
-                fontWeight: "500",
-                marginLeft: "0.5rem",
-                marginRight: "0.5rem",
-                padding: "0.5rem 0.75rem",
-                whiteSpace: "nowrap",
-              }}
             >
               SERVICIOS
             </Nav.Link>
             <Nav.Link
               href="#historia"
               className="text-white linksitos"
-              style={{
-                fontSize: "1rem",
-                fontWeight: "500",
-                marginLeft: "0.5rem",
-                marginRight: "0.5rem",
-                padding: "0.5rem 0.75rem",
-                whiteSpace: "nowrap",
-              }}
             >
               HISTORIA
             </Nav.Link>
             <Nav.Link
               href="#historia"
               className="text-white linksitos"
-              style={{
-                fontSize: "1rem",
-                fontWeight: "500",
-                marginLeft: "0.5rem",
-                marginRight: "0.5rem",
-                padding: "0.5rem 0.75rem",
-                whiteSpace: "nowrap",
-              }}
             >
               PROYECTOS
             </Nav.Link>
             <Nav.Link
               href="#equipamiento"
               className="text-white linksitos"
-              style={{
-                fontSize: "1rem",
-                fontWeight: "500",
-                marginLeft: "0.5rem",
-                marginRight: "0.5rem",
-                padding: "0.5rem 0.75rem",
-                whiteSpace: "nowrap",
-              }}
             >
               EQUIPAMIENTO
             </Nav.Link>
             <Nav.Link
               href="#contacto"
               className="text-white linksitos"
-              style={{
-                fontSize: "1rem",
-                fontWeight: "500",
-                marginLeft: "0.5rem",
-                marginRight: "0.5rem",
-                padding: "0.5rem 0.75rem",
-                whiteSpace: "nowrap",
-              }}
             >
               CONTACTO
             </Nav.Link>
